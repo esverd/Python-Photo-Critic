ec16d04cd5d1de32dfd649d264e4999f5bc7b9cf
Sverd Industries <eirik.sverd@gmail.com>
2024-12-30 11:11:40 -0600

    moving to local notebook dev

--- tasks.py
+++
@@ -1,8 +0,0 @@
-
-task_hello_world = Task(
-    description=(
-        """Print a poem on 5 lines, detailing the struggles of debugging code"""
-    ),
-    expected_output="""A poem, 5 lines""",
-    agent=agent_image_extractor
-)
